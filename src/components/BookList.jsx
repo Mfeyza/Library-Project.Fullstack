64ec49763c04922f3e88c3ac02a7a73f3073d1a3
M. Feyza Yıldırım <144602340+Mfeyza@users.noreply.github.com>
2024-03-08 17:12:29 +0300

    end

---
+++ src/components/BookList.jsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import axios from 'axios';
+
+export default function BookList() {
+    const [books, setBooks] = React.useState([]);
+    React.useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await axios.get('http://127.0.0.1:8000/');
+            setBooks(response.data.result.rows);
+          } catch (error) {
+            console.error('GET isteğinde hata meydana geldi:', error);
+          }
+        };
+    
+        fetchData(); 
+      }, []);
+
+
+  return (
+    <>
+      {books.map((item) => (
+        <Card sx={{ maxWidth: 345 }} key={item.id}> 
+          <CardMedia
+            sx={{ height: 140 }}
+            image={item.image}
+            title="bookImage"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {item.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item.author}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item.genre}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item.ISBN}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item.publicationYear}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Delete</Button>
+            <Button size="small">Edit</Button>
+          </CardActions>
+        </Card>
+      ))}
+    </>
+  );
+}
---
+++ src/components/NewBook.jsx
@@ -0,0 +1,113 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
+import {
+  Box,
+  Button,
+  CardActionArea,
+  CardActions,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import genre from "../helper/data";
+
+
+export default function MultiActionAreaCard() {
+    const [formData, setFormData] = React.useState({
+      title: '',
+      author: '',
+      ISBN: '',
+      publicationYear: '',
+      genre: '',
+      image: ''
+    });
+  
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+      setFormData(info => ({
+        ...info,
+        [name]: value
+      }));
+    };
+  
+    const handleSubmit = async () => {
+      try {
+        const response = await axios.post('http://127.0.0.1:8000/', formData);
+        console.log(response.data);
+       return response
+      } catch (error) {
+        console.error('POST isteğinde hata:', error);
+      }
+    };
+  
+    return (
+      <Card sx={{ maxWidth: 345, mt: 5 }}>
+         <form onSubmit={handleSubmit}>
+        <CardContent>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+            <TextField
+              name="title"
+              label="Book Title"
+              value={formData.title}
+              onChange={handleChange}
+            />
+            <TextField
+              name="author"
+              label="Author"
+              value={formData.author}
+              onChange={handleChange}
+            />
+            <TextField
+              name="ISBN"
+              label="ISBN"
+              value={formData.ISBN}
+              onChange={handleChange}
+            />
+            <TextField
+              name="publicationYear"
+              label="Publication Year"
+              type="number"
+              value={formData.publicationYear}
+              onChange={handleChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <Select
+              name="genre"
+              value={formData.genre}
+              label="Genre"
+              onChange={handleChange}
+              displayEmpty
+              inputProps={{ 'aria-label': 'Without label' }}
+            >
+              <MenuItem value="">
+                <em>Genre</em>
+              </MenuItem>
+              {genre.map((g, index) => (
+                <MenuItem key={index} value={g}>{g}</MenuItem>
+              ))}
+            </Select>
+            <TextField
+              name="image"
+              label="Image URL"
+              type="url"
+              value={formData.image}
+              onChange={handleChange}
+            />
+          </Box>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary" type="submit">
+            Add Book
+          </Button>
+        </CardActions>
+        </form>
+      </Card>
+    );
+  }
